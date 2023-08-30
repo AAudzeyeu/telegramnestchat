de30521c943a611ca17f7517a6ce04713a255656
Artyom <128896821+AAudzeyeu@users.noreply.github.com>
2023-08-30 17:37:58 +0300

    init chatbot telegram text

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TelegramService } from './telegram.service';
+import {TelegrafModule} from "nestjs-telegraf";
+import {options} from "./telegram-confif.factory";
 
 @Module({
+  imports: [TelegrafModule.forRootAsync(options())],
   providers: [TelegramService]
 })
 export class TelegramModule {}
--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import {Ctx, Message, On, Start, Update} from "nestjs-telegraf";
+import {Telegraf, Scenes} from "telegraf";
 
-@Injectable()
-export class TelegramService {}
+type Context = Scenes.SceneContext;
+
+@Update()
+export class TelegramService extends  Telegraf<Context> {
+    @Start()
+    onStart(@Ctx() ctx: Context) {
+        ctx.replyWithHTML(`<b>Привет, ${ctx.from.username}</b>
+        Это чат бот от Томаса!
+        Вводи любую фразу и получи ответ!
+        `);
+    }
+
+    @On('text')
+    onMessage(@Message('text') message: string, @Ctx() ctx: Context) {
+        ctx.replyWithHTML(`<i>${message}</i>`);
+    }
+
+}
